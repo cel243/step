aeaf07ef8ddb5b7129b470153fd7a6c9b1e925c1
Caroline Lui <cel243@cornell.edu>
2020-06-05 19:29:03 +0000

    Split constants into two files

---
+++ portfolio/src/main/java/com/google/sps/data/EntityProperties.java
@@ -0,0 +1,14 @@
+/** File containing constants used throughout project. */
+public final class EntityProperties {
+  /* Entity Properties: */
+
+  /** The property representing the text of a comment on the webpage. */
+  public static final String COMMENT_TEXT = "text";
+
+  /** The property representing the name of the author of a comment. */
+  public static final String COMMENT_AUTHOR = "name";
+
+  /** The property representing the timestamp when the comment was posted (in 
+    * milliseconds since the epoch). */
+  public static final String COMMENT_TIMESTAMP = "time";
+}
--- portfolio/src/main/java/com/google/sps/data/RequestParameters.java
+++ portfolio/src/main/java/com/google/sps/data/RequestParameters.java
@@ -1,17 +1,5 @@
 /** File containing constants used throughout project. */
-public final class Constants {
-  /* Entity Properties: */
-
-  /** The property representing the text of a comment on the webpage. */
-  public static final String COMMENT_TEXT = "text";
-
-  /** The property representing the name of the author of a comment. */
-  public static final String COMMENT_AUTHOR = "name";
-
-  /** The property representing the timestamp when the comment was posted (in 
-    * milliseconds since the epoch). */
-  public static final String COMMENT_TIMESTAMP = "time";
-
+public final class RequestParameters {
   /* Request Parameters for DataServlet POST: */
 
   /** The text of a submitted comment. */
