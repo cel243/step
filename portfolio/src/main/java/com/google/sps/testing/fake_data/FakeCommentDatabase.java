f7ea690e6e21b284b23c6fab0cb1fd984b373d61
Caroline Lui <cel243@cornell.edu>
2020-06-17 15:34:30 +0000

    Fix delete logic string conversion

--- portfolio/src/main/java/com/google/sps/testing/fake_data/FakeCommentDatabase.java
+++ portfolio/src/main/java/com/google/sps/testing/fake_data/FakeCommentDatabase.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 
 /** A fake database that stored comments for testing purposes. */
 public class FakeCommentDatabase {
-  private static LinkedList<FakeComment> comments = new LinkedList<FakeComment>();
+  private static List<FakeComment> comments = new LinkedList<FakeComment>();
   private static int next_id = 0;
 
   /** 
@@ -16,7 +16,7 @@ public class FakeCommentDatabase {
     * sentiment, and topic. 
     */
   public static void put(FakeComment comment) {
-    comments.add(new FakeComment(comment.text, "", comment.time, next_id, 
+    comments.add(0, new FakeComment(comment.text, "", comment.time, next_id, 
       comment.userId, comment.email, "POSITIVE", ""));
     next_id++;
   }
@@ -40,25 +40,19 @@ public class FakeCommentDatabase {
     * user, or all the comments if userId = "ADMIN". 
     */
   public static void deleteAllCommentsByUser(String userId) {
-    int i = 0;
-    for (FakeComment comment : comments) {
-      if (userId.equals("ADMIN") || userId.equals(comment.userId)) {
-        comments.remove(i);
-      } else {
-        i++;
-      }
+    if (userId.equals("ADMIN")) {
+      comments = new LinkedList<FakeComment>();
+    } else {
+      comments = comments.stream()
+        .filter(comment -> !userId.equals(comment.userId))
+        .collect(Collectors.toList());
     }
   }
 
   /** Deletes the comment with id commentId from the fake database. */
   public static void deleteThisComment(String commentId) {
-    int i = 0;
-    for (FakeComment comment : comments) {
-      if (commentId.equals(comment.id)) {
-        comments.remove(i);
-        break;
-      }
-      i++;
-    }
+    comments = comments.stream()
+      .filter(comment -> !commentId.equals(comment.id+""))
+      .collect(Collectors.toList());
   }
 }
